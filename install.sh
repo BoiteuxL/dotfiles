06fcf18b2bfa2a003ab6f3063cc27af8b2968bac
Loïc Boiteux <loicboiteux@protonmail.com>
2025-06-09 12:50:32 -0400

    feat: ✨ New install script

--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# ZSH
+
+echo "Installing ZSH with Starship..."
+sudo pacman -S zsh starship --noconfirm
+
+chsh -s "/bin/zsh" # User
+sudo chsh -s "/bin/zsh" # Root
+
+# VSCode
+
 while IFS= read -r extension; do
     code --install-extension $extension --force
 done < vscode-extensions
 
+
+# GNU Stow
+
 echo "Running GNU Stow..."
 stow .
+
+
+echo "Done!"
