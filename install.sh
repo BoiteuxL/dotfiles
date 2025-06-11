5b3fcb35c820746f9a262cc324da11f4ab0acfb3
Loïc Boiteux <loicboiteux@protonmail.com>
2025-06-10 20:49:18 -0400

    Fixed config

--- install.sh
+++ install.sh
@@ -3,19 +3,12 @@
 # ZSH
 
 echo "Installing ZSH with Starship..."
-sudo dnf install zsh starship stow --noconfirm
+sudo dnf install zsh stow
 
 curl -sS https://starship.rs/install.sh | sh
 
 chsh -s "/usr/bin/zsh" # User
 sudo chsh -s "/usr/bin/zsh" # Root
-
-# KDE Theme
-#git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde
-#./install.sh
-#cd .. && rm -fr ./catppuccin-kde
-
-curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 - > /dev/null
 
 # VSCode
 while IFS= read -r extension; do
@@ -24,7 +17,6 @@ done < vscode-extensions
 
 
 # GNU Stow
-
 echo "Running GNU Stow..."
 stow .
 
