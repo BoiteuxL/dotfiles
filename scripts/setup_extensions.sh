4ae41286b999b6cb5cf5763676d9214832717d42
Loïc Boiteux <loic.boiteux@protonmail.com>
2025-07-27 23:30:23 -0400

    Added extensions + SuperT shortcut

--- scripts/setup_extensions.sh
+++ scripts/setup_extensions.sh
@@ -289,8 +289,11 @@ log_command "Configuring extension: Runcat"
 gsettings set org.gnome.shell.extensions.runcat displaying-items 'character-only'
 gsettings set org.gnome.shell.extensions.runcat idle-threshold 10
 
-# Runcat
-log_item "Runcat"
+
+
+
+# Arch Linux Updates Indicator
+log_item "Arch Linux Updates Indicator"
 log_command "Installing extension: Arch Linux Updates Indicator"
 gext install arch-update@RaphaelRochet
 
@@ -301,5 +304,120 @@ log_command "Configuring extension: Arch Linux Updates Indicator"
 gsettings set org.gnome.shell.extensions.arch-update always-visible false
 gsettings set org.gnome.shell.extensions.arch-update enable-positioning true
 gsettings set org.gnome.shell.extensions.arch-update position-number 1
-gsettings set org.gnome.shell.extensions.arch-update update-cmd "ptyxis --new-window -x '/bin/sh -c "yay -Syu ; echo Done - Press enter to exit; read _"'
+#gsettings set org.gnome.shell.extensions.arch-update update-cmd "ptyxis --new-window -x '/bin/sh -c "yay -Syu ; echo Done - Press enter to exit; read _"'
 gsettings set org.gnome.shell.extensions.arch-update use-buildin-icons false
+
+
+# Rounded Window Corners
+log_item "Rounded Window Corners"
+log_command "Installing extension: Rounded Window Corners"
+gext install rounded-window-corners@fxgn
+log_command "Importing schema for: Rounded Window Corners"
+sudo p $HOME/.local/share/gnome-shell/extensions/rounded-window-corners@fxgn/schemas/org.gnome.shell.extensions.rounded-window-corners-reborn.gschema.xml /usr/share/glib-2.0/schemas && sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+log_command "Configuring extension: Rounded Window Corners"
+# Nothing to do here, the extension has no settings to configure.
+
+
+
+# Blur My Shell
+log_item "Blur My Shell"
+log_command "Installing extension: Blur My Shell"
+gext install blur-my-shell@aunetx
+log_command "Importing schema for: Blur My Shell"
+sudo cp $HOME/.local/share/gnome-shell/extensions/blur-my-shell@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas && sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+log_command "Configuring extension: Blur My Shell"
+org.gnome.shell.extensions.blur-my-shell brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell color-and-noise true
+org.gnome.shell.extensions.blur-my-shell debug false
+org.gnome.shell.extensions.blur-my-shell hacks-level 1
+org.gnome.shell.extensions.blur-my-shell noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell pipelines {'pipeline_default': {'name': <'Default'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000000'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>]>}, 'pipeline_default_rounded': {'name': <'Default rounded'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000001'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>, <{'type': <'corner'>, 'id': <'effect_000000000002'>, 'params': <{'radius': <24>}>}>]>}}
+org.gnome.shell.extensions.blur-my-shell settings-version 2
+org.gnome.shell.extensions.blur-my-shell sigma 30
+org.gnome.shell.extensions.blur-my-shell.appfolder blur true
+org.gnome.shell.extensions.blur-my-shell.appfolder brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.appfolder color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.appfolder customize false
+org.gnome.shell.extensions.blur-my-shell.appfolder noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.appfolder noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.appfolder sigma 17
+org.gnome.shell.extensions.blur-my-shell.appfolder style-dialogs 1
+org.gnome.shell.extensions.blur-my-shell.applications blacklist ['Plank', 'com.desktop.ding', 'Conky']
+org.gnome.shell.extensions.blur-my-shell.applications blur false
+org.gnome.shell.extensions.blur-my-shell.applications blur-on-overview false
+org.gnome.shell.extensions.blur-my-shell.applications brightness 1.0
+org.gnome.shell.extensions.blur-my-shell.applications color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.applications customize true
+org.gnome.shell.extensions.blur-my-shell.applications dynamic-opacity true
+org.gnome.shell.extensions.blur-my-shell.applications enable-all false
+org.gnome.shell.extensions.blur-my-shell.applications noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.applications noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.applications opacity 215
+org.gnome.shell.extensions.blur-my-shell.applications sigma 30
+org.gnome.shell.extensions.blur-my-shell.applications whitelist @as []
+org.gnome.shell.extensions.blur-my-shell.coverflow-alt-tab blur true
+org.gnome.shell.extensions.blur-my-shell.coverflow-alt-tab pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur false
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock corner-radius 12
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock customize false
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock override-background true
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock pipeline 'pipeline_default_rounded'
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock sigma 30
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock static-blur true
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock style-dash-to-dock 0
+org.gnome.shell.extensions.blur-my-shell.dash-to-dock unblur-in-overview false
+org.gnome.shell.extensions.blur-my-shell.dash-to-panel blur-original-panel true
+org.gnome.shell.extensions.blur-my-shell.hidetopbar compatibility false
+org.gnome.shell.extensions.blur-my-shell.lockscreen blur true
+org.gnome.shell.extensions.blur-my-shell.lockscreen brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.lockscreen color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.lockscreen customize false
+org.gnome.shell.extensions.blur-my-shell.lockscreen noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.lockscreen noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.lockscreen pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.lockscreen sigma 30
+org.gnome.shell.extensions.blur-my-shell.overview blur true
+org.gnome.shell.extensions.blur-my-shell.overview brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.overview color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.overview customize false
+org.gnome.shell.extensions.blur-my-shell.overview noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.overview noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.overview pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.overview sigma 30
+org.gnome.shell.extensions.blur-my-shell.overview style-components 1
+org.gnome.shell.extensions.blur-my-shell.panel blur false
+org.gnome.shell.extensions.blur-my-shell.panel brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.panel color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.panel customize false
+org.gnome.shell.extensions.blur-my-shell.panel force-light-text false
+org.gnome.shell.extensions.blur-my-shell.panel noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.panel noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.panel override-background true
+org.gnome.shell.extensions.blur-my-shell.panel override-background-dynamically false
+org.gnome.shell.extensions.blur-my-shell.panel pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.panel sigma 30
+org.gnome.shell.extensions.blur-my-shell.panel static-blur true
+org.gnome.shell.extensions.blur-my-shell.panel style-panel 0
+org.gnome.shell.extensions.blur-my-shell.panel unblur-in-overview true
+org.gnome.shell.extensions.blur-my-shell.screenshot blur true
+org.gnome.shell.extensions.blur-my-shell.screenshot brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.screenshot color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.screenshot customize false
+org.gnome.shell.extensions.blur-my-shell.screenshot noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.screenshot noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.screenshot pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.screenshot sigma 30
+org.gnome.shell.extensions.blur-my-shell.window-list blur true
+org.gnome.shell.extensions.blur-my-shell.window-list brightness 0.59999999999999998
+org.gnome.shell.extensions.blur-my-shell.window-list color (0.0, 0.0, 0.0, 0.0)
+org.gnome.shell.extensions.blur-my-shell.window-list customize false
+org.gnome.shell.extensions.blur-my-shell.window-list noise-amount 0.0
+org.gnome.shell.extensions.blur-my-shell.window-list noise-lightness 0.0
+org.gnome.shell.extensions.blur-my-shell.window-list pipeline 'pipeline_default'
+org.gnome.shell.extensions.blur-my-shell.window-list sigma 30
