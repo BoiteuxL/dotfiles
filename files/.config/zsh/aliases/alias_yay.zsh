f69793605a0c095d93608e61eaa7b8f84744b7b3
Loïc Boiteux <loic.boiteux@protonmail.com>
2025-08-05 09:50:57 -0400

    Added aliases for mirrors updating

--- files/.config/zsh/aliases/alias_yay.zsh
+++ files/.config/zsh/aliases/alias_yay.zsh
@@ -1,5 +1,8 @@
 
 # Yay Aliases
-alias yayu='yay -Syu'
+# alias yay='yay'
+
 alias yayi='yay -S'
 alias yayr='yay -Rns'
+alias yayma='sudo reflector --protocol https --verbose --latest 25 --sort rate --save /etc/pacman.d/mirrorlist && yay -Syyu'
+alias yayme='sudo eos-rankmirrors --verbose && yay -Syyu'
