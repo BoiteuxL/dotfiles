6884ae7154272a9ec076c441dc01bf6a92036ce3
Loïc Boiteux <loic.boiteux@protonmail.com>
2026-04-06 22:43:07 -0400

    Edited installation process

--- files/.config/quickshell/shell.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick // for Text
-import Quickshell // for PanelWindow
-import Quickshell.Io // for Process
-
-import "modules"
-
-ShellRoot {
-    Bar {}
-}
--- install.sh
+++ install.sh
@@ -5,15 +5,9 @@ cd $SCRIPT_DIR
 
 
 echo "Installing dependencies"
-yay -S --needed $(cat ./dependencies)
+yay -Syy --needed $(cat ./dependencies)
 
 cd files
 echo "Linking dotfiles"
 stow . -t $HOME --verbose
 ln -sf "$HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
-
-echo "Downloading GTK theme"
-sudo pacman -S gtk-engine-murrine
-git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git --depth=1
-./Catppuccin-GTK-Theme/themes/install.sh -l -c dark -t lavender --tweaks macos 
-rm -rf ./Catppuccin-GTK-Theme
